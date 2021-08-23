d316e2539c6d97fd84d496b619d078702d6c06e1
Samuel Heal <samuelheal@hotmail.com>
2021-08-23 16:00:56 +1000

    professional changes

--- src/components/body/about/index.js
+++ src/components/body/about/index.js
@@ -1,43 +1,48 @@
-import React from 'react'
-import './about.css'
-import samPic from './sam-pic.jpeg'
-import resume from '../../../files/Developer Resume.pdf'
+import React from 'react';
+import './about.css';
+import samPic from './sam-pic.jpeg';
+import resume from '../../../files/Developer Resume.pdf';
 
 function About() {
-    return (
-        <div className='aboutContainer'>
-            <div id='about'></div>
-            <div className="aboutHeading">
-                About
-            </div>
-            <div className='aboutContentContainer'>
-                <div className="aboutPara">
-                    I am a coding fanatic who has recently begun a journey in Web Development 
-                    with the dream of becoming a fully fledged Software Engineer. I am incredibly 
-                    passionate about what I do and am nearing the completion of a six month bootcamp 
-                    in Web Development that has been hosted by Trilogy Education through the University 
-                    of Sydney. Here, I have learnt the necessary skills required to become a Full-Stack 
-                    Web Developer. To further add to my education, I have been accepted into a Bachelor 
-                    of Design Computing/Advanced Studies, the Advanced Studies component will be used to 
-                    complete a Major in Software Development. I am beyond excited to see where my love 
-                    for this industry takes me.
-                    <div className='btnContainer'>
-                        <a className="waterBox" href={resume} target='_blank' rel="noreferrer">
-                            <span className="waterText">Resume</span>
-                            <div className="water"></div>
-                        </a>
-                    </div>
-                    
-                </div>
-                <div className='picContainer'>
-                    <img className="aboutPic" src={samPic} alt='Sam'></img>
-                </div>
-                
-                <div id='skills'></div>
-            </div>
-           
-        </div> 
-    )
+  return (
+    <div className='aboutContainer'>
+      <div id='about'></div>
+      <div className='aboutHeading'>About</div>
+      <div className='aboutContentContainer'>
+        <div className='aboutPara'>
+          My name is Samuel Heal, a Full-Stack web developer who has recently
+          earned a certificate in this field for the completion of the Coding
+          Bootcamp hosted by Trilogy Education and the University of Sydney.
+          Here, I have learnt the necessary skills required to become a
+          competent Full-Stack Web Developer. I am a coding fanatic who is
+          constantly seeking new ways to grow and thrive within this industry. I
+          place my focus on building projects that I believe can make people’s
+          lives even slightly easier. My latest project, aDevelopersFriend, is
+          designed for web developers in need of a place to easily store their
+          code. Built using the MERN framework, It is capable of creating
+          projects that can store the folders, front-end files and back-end
+          files that allow users to replicate entire project file structures if
+          they need. Check it out in my projects section below!
+          <div className='btnContainer'>
+            <a
+              className='waterBox'
+              href={resume}
+              target='_blank'
+              rel='noreferrer'
+            >
+              <span className='waterText'>Resume</span>
+              <div className='water'></div>
+            </a>
+          </div>
+        </div>
+        <div className='picContainer'>
+          <img className='aboutPic' src={samPic} alt='Sam'></img>
+        </div>
+
+        <div id='skills'></div>
+      </div>
+    </div>
+  );
 }
 
-export default About
+export default About;
--- src/components/body/skills/index.js
+++ src/components/body/skills/index.js
@@ -1,56 +1,55 @@
-import React from 'react'
-import './skills.css'
+import React from 'react';
+import './skills.css';
 
 function Skills() {
-    return (
-        <div className='skillsContainer'>
-            <div className="skillsHeading">
-                Skills
-            </div>
-            <div className='allSkills'>
-                <div className='skillTypeContainer'>
-                    <div className='skillTypeHeading'>
-                        <p>Languages</p>
-                    </div>
-                    <div className='skillNames'>
-                        <p>HTML</p>
-                        <p>CSS</p>
-                        <p>Javascript</p>
-                        <p>mySQL</p>
-                        <p>Python</p>
-                    </div>
-                </div>
-                <div className='skillTypeContainer'>
-                    <div className='skillTypeHeading'>
-                        <p>Technologies</p>
-                    </div>
-                    <div className='skillNames'>
-                        <p>Bootstrap</p>
-                        <p>JQuery</p>
-                        <p>Node.js</p>
-                        <p>Sequalize</p>
-                        <p>Express.js</p>
-                        <p>Handlebars.js</p>
-                        <p>mongoDB</p>
-                        <p>React.js</p>
-                        <p>Anime.js</p>
-                        <p>noSQL</p>
-                    </div>
-                </div>
-                    
-                <div className='skillTypeContainer'>
-                    <div className='skillTypeHeading'> 
-                        <p>Development Frameworks</p>
-                    </div>
-                    <div className='skillNames'>
-                        <p>Model-View-Controller Paradigm</p>
-                        <p>MERN-Stack</p>
-                    </div>
-                </div>
-            </div>
-            <div id='projects'></div>
+  return (
+    <div className='skillsContainer'>
+      <div className='skillsHeading'>Skills</div>
+      <div className='allSkills'>
+        <div className='skillTypeContainer'>
+          <div className='skillTypeHeading'>
+            <p>Languages</p>
+          </div>
+          <div className='skillNames'>
+            <p>HTML</p>
+            <p>CSS</p>
+            <p>Javascript</p>
+            <p>mySQL</p>
+            <p>graphQL</p>
+            <p>Python</p>
+          </div>
         </div>
-    )
+        <div className='skillTypeContainer'>
+          <div className='skillTypeHeading'>
+            <p>Technologies</p>
+          </div>
+          <div className='skillNames'>
+            <p>Bootstrap</p>
+            <p>JQuery</p>
+            <p>Node.js</p>
+            <p>Sequalize</p>
+            <p>Express.js</p>
+            <p>Handlebars.js</p>
+            <p>mongoDB</p>
+            <p>React.js</p>
+            <p>Anime.js</p>
+            <p>noSQL</p>
+          </div>
+        </div>
+
+        <div className='skillTypeContainer'>
+          <div className='skillTypeHeading'>
+            <p>Development Frameworks</p>
+          </div>
+          <div className='skillNames'>
+            <p>Model-View-Controller Paradigm</p>
+            <p>MERN-Stack</p>
+          </div>
+        </div>
+      </div>
+      <div id='projects'></div>
+    </div>
+  );
 }
 
-export default Skills
+export default Skills;
